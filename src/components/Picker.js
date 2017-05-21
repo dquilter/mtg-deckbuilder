4558eb46eb5578e4580459ec7cefdee6fe09096e
dquilter <david_quilter@hotmail.com>
2017-05-21 14:58:41 +0100

    Displaying MTG API data

--- src/components/Picker.js
+++ src/components/Picker.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import Mtg from 'mtgsdk';
-import TestData from './TestData';
 import PickerListItem from './PickerListItem';
 import PickerForm from './PickerForm';
 
@@ -10,36 +8,10 @@ class Picker extends Component {
     super(props);
 
     this.cardList = this.cardList.bind(this);
-    this.getCard = this.getCard.bind(this);
-  }
-
-  getCard() {
-    // Mtg.card.find(3)
-    // .then(result => {
-    //     console.log(result.card) // "Black Lotus"
-    // });
-
-    // Mtg.card.where({ name: 'Fatal Push' })
-    // .then(cards => {
-    //     console.log(cards) // "Squee, Goblin Nabob"
-    // })
-    // Mtg.card.where({ name: 'Zada' })
-    // .then(cards => {
-    //     console.log(cards) // "Squee, Goblin Nabob"
-    // })
-    // Mtg.card.where({ name: 'Call for Unity' })
-    // .then(cards => {
-    //     console.log(cards) // "Squee, Goblin Nabob"
-    // })
-    console.log(TestData);
-    return TestData;
-
   }
 
   cardList() {
     let result = [];
-      console.log(this.props)
-      console.log(this.props.cardList)
     for (var key in this.props.cardList) {
       let card = this.props.cardList[key];
       console.log(this.props.cardList[card])
@@ -60,7 +32,9 @@ class Picker extends Component {
         <PickerForm 
           addCard={this.props.addCard}
         />
-        {this.cardList()}
+        <div className="picker-table">
+          {this.cardList()}
+        </div>
       </div>
     )
   }
--- src/components/PickerForm.js
+++ src/components/PickerForm.js
@@ -1,21 +1,29 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Mtg from 'mtgsdk';
+import TestData from './TestData';
 
 class PickerForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      inputVal: ''
+      inputVal: '',
+      searchResults: null
     }    
 
     this.updateInputVal = this.updateInputVal.bind(this);
     this.addCard = this.addCard.bind(this);
+    this.getCard = this.getCard.bind(this);
+    this.testSearchReady = this.testSearchReady.bind(this);
+    this.selectCard = this.selectCard.bind(this);
   }
 
   updateInputVal(evt) {
     this.setState({
       inputVal: evt.target.value
     })
+
+    this.testSearchReady(evt.target.value);
   }
 
   addCard() {
@@ -24,10 +32,67 @@ class PickerForm extends Component {
     // this.props.addCard(results[0]);
   }
 
+  searchTimeout = undefined;
+  
+  testSearchReady(value) {
+    clearTimeout(this.searchTimeout);
+    this.searchTimeout = setTimeout(function() {
+      this.getCard(value);
+    }.bind(this), 800);
+  }
+
+  getCard(value) {
+    // Mtg.card.find(3)
+    // .then(result => {
+    //     console.log(result.card) // "Black Lotus"
+    // });
+
+    // Mtg.card.where({ name: 'Fatal Push' })
+    // .then(cards => {
+    //     console.log(cards) // "Squee, Goblin Nabob"
+    // })
+    // Mtg.card.where({ name: 'Zada' })
+    // .then(cards => {
+    //     console.log(cards) // "Squee, Goblin Nabob"
+    // })
+    // Mtg.card.where({ name: 'Call for Unity' })
+    // .then(cards => {
+    //     console.log(cards) // "Squee, Goblin Nabob"
+    // })
+    // console.log(TestData);
+    // return TestData;
+    Mtg.card.where({ name: value })
+    .then(cards => {
+        console.log(cards) // "Squee, Goblin Nabob"
+        this.setState({
+          searchResults: cards
+        })
+        console.log(this.state)
+    })
+  }
+
+  searchResults() {
+    let resultList = [];
+    for (var key in this.state.searchResults) {
+      let result = this.state.searchResults[key];
+      resultList.push(<a href="#" key={key} onClick={this.selectCard}>{result.name}</a>)
+    }
+    var open = resultList.length > 0 ? 'is-open' : '';
+    return (
+      <div className={`search-results ${open}`}>
+        {resultList}
+      </div>
+    )
+  }
+
+  selectCard() {
+
+  }
+
   
   render() {
     return (
-      <div className="form-row clearfix ">
+      <div className="picker-form clearfix ">
         <input 
           type="text" 
           className="card-input" 
@@ -35,13 +100,14 @@ class PickerForm extends Component {
           onChange={this.updateInputVal}
         />
         <button onClick={this.addCard}>Add Card</button>
+        {this.searchResults()}
       </div>      
     )
   }
 }
 
 PickerForm.PropTypes = {
-
+  addCard: PropTypes.func.isRequired
 }
 
 export default PickerForm;
